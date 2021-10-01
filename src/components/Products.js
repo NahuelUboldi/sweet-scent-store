d85aca413d032e4c1aa61c07af0ea14aa92e546b
Nahuel Uboldi <nahueluboldi@gmail.com>
2021-10-01 00:27:27 -0300

    update

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { store } from './state/store';
 
 function Products() {
+  const currentState = store.getState();
   return (
-    <section id='products'>
+    <section className='products'>
       <div className='container'>
         <h2 className='display-2'>Products</h2>
       </div>
---
+++ src/components/micro-components/Product.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Product() {
+  return <div>Producto</div>;
+}
+
+export default Product;
